0da4a5929bd248ada970bb56b1135303564014c2
Martins Obayomi <obayomimartins96@gmail.com>
2022-01-12 14:07:17 +0100

    Npm module boilerplate - publish on npm

---
+++ npm-module-setup/src/index.js
@@ -0,0 +1,3 @@
+export { default as Component } from './Component';
+// Import your reuseable component and export it out of index so it can be accessible from outside this module
+// Component is a placeholder for your component(s)
